66b9162106e442fff13a2e1bc112b4bbe3e069d6
Tianchen Jin <tianchenjin@gmail.com>
2021-04-11 23:37:08 -0700

    Added a working DFS solution

--- nacbrac.py
+++ nacbrac.py
@@ -1,14 +1,16 @@
 import pdb
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from enum import Enum, auto
-from typing import List, Union
+from dataclasses import dataclass, field
+from enum import Enum, unique
+from typing import List, Union, Set
+import datetime
 
+@unique
 class Suit(Enum):
-    Heart = auto()
-    Diamond = auto()
-    Spade = auto()
-    Club = auto()
+    Heart = "H"
+    Diamond = "D"
+    Spade = "S"
+    Club = "C"
 
 @dataclass
 class Card:
@@ -24,6 +26,15 @@ class Card:
     def is_black(self):
         return self.suit == Suit.Spade or self.suit == Suit.Club
 
+    def __str__(self) -> str:
+        return str(self.value) + self.suit.value
+
+    @staticmethod
+    def from_str(source: str):
+        value_str = source[:-1]
+        suit_str = source[-1]
+        return Card(value=int(value_str), suit=Suit(suit_str))
+
 class WildcardSlot:
     def __init__(self, card: Card=None):
         self.card = card
@@ -31,12 +42,12 @@ class WildcardSlot:
     def peek(self) -> Card:
         return self.card
 
-    def get(self) -> Card:
+    def pop(self) -> Card:
         card = self.card
         self.card = None
         return card
 
-    def put(self, card: Card) -> bool:
+    def push(self, card: Card) -> bool:
         if self.card is not None:
             return False
         else:
@@ -46,19 +57,42 @@ class WildcardSlot:
     def has_card(self) -> bool:
         return self.card is not None
 
+    def __str__(self):
+        return str(self.card) if self.has_card() else "_"
+
+    @staticmethod
+    def from_str(source: str):
+        if source == "_":
+            return WildcardSlot()
+        else:
+            return WildcardSlot(Card.from_str(source))
+
 @dataclass
 class Move:
-    before: Union[WildcardSlot, int]
-    after: Union[WildcardSlot, int]
-    cards: List[Card]
+    before: int # negative means wildcard slot
+    after: int # negative means wildcard slot
+    num_cards: int
 
-@dataclass
+    def is_before_wildcard_slot(self) -> bool:
+        return self.before < 0
+
+    def is_after_wildcard_slot(self) -> bool:
+        return self.after < 0
+
+def empty_field_slots():
+    return [[], [], [], [], [], [], [], [], []]
+
+@dataclass(unsafe_hash=True)
 class Gameboard:
     wildcard_slot: WildcardSlot = WildcardSlot()
-    field_slots: List[List[Card]] = [[], [], [], [], [], [], [], [], []]
+    field_slots: List[List[Card]] = field(default_factory=empty_field_slots)
 
     def validate(self) -> bool:
+        if len(self.field_slots) != 9:
+            return False
+
         cards: List[Card] = []
+        # count all cards
         if self.wildcard_slot.has_card():
             cards.append(self.wildcard_slot.peek())
         for card_stack in self.field_slots:
@@ -85,19 +119,118 @@ class Gameboard:
         return True
 
     def execute(self, move: Move) -> None:
-        pass
+        # print("exec: {}".format(repr(move)))
+        try:
+            if move.is_after_wildcard_slot():
+                card = self.field_slots[move.before].pop()
+                self.wildcard_slot.push(card)
+            elif move.is_before_wildcard_slot():
+                card = self.wildcard_slot.pop()
+                self.field_slots[move.after].append(card)
+            else:
+                cards = self.field_slots[move.before][-move.num_cards:]
+                self.field_slots[move.after].extend(cards)
+                before_list = self.field_slots[move.before]
+                self.field_slots[move.before] = before_list[:(len(before_list)-move.num_cards)]
+        except:
+            pdb.set_trace()
 
     def undo(self, move: Move) -> None:
-        pass
+        # print("undo: {}".format(repr(move)))
+        if move.is_after_wildcard_slot():
+            card = self.wildcard_slot.pop()
+            self.field_slots[move.before].append(card)
+        elif move.is_before_wildcard_slot():
+            card = self.field_slots[move.after].pop()
+            self.wildcard_slot.push(card)
+        else:
+            cards = self.field_slots[move.after][-move.num_cards:]
+            self.field_slots[move.before].extend(cards)
+            after_list = self.field_slots[move.after]
+            self.field_slots[move.after] = after_list[:(len(after_list)-move.num_cards)]
 
     def solved(self) -> bool:
         if self.wildcard_slot.has_card():
             return False
-        
-        pass
+        return all([self._is_field_slot_done(field_slot) for field_slot in self.field_slots])
 
-    def get_valid_moves(self) -> List[Move]:
-        pass
+    def get_field_slot_moves(self) -> List[Move]:
+        """
+        generate field slot moves first, then consider wildcard slot
+        """
+        valid_moves: List[Move] = []
+        for from_idx in range(0, 9):
+            # skip if the field slot is already sorted (including empty slot)
+            if self._is_field_slot_done(self.field_slots[from_idx]):
+                continue
+
+            for to_idx in range(0, 9):
+                if from_idx == to_idx:
+                    continue
+
+                from_slot = self.field_slots[from_idx]
+                to_slot = self.field_slots[to_idx]
+                for num_cards in range(len(from_slot), 0, -1):
+                    cards_to_move = from_slot[-num_cards:]
+                    if self._can_move_cards(cards_to_move, to_slot):
+                        # don't move the whole stack to empty slot and leave an empty slot
+                        # don't move values from sub-stack to another sub-stack (noop)
+                        if (not (not to_slot and num_cards == len(from_slot))) \
+                            and (not (not from_slot[-num_cards].is_face() and len(to_slot) > 0 and num_cards < len(from_slot) and from_slot[-(num_cards+1)].value == to_slot[-1].value)):
+                            valid_moves.append(Move(from_idx, to_idx, num_cards))
+                        # move the whole stack, ignore partial moves
+                        break
+        return valid_moves
+
+    def get_wildcard_slot_moves(self) -> List[Move]:
+        valid_moves: List[Move] = []
+        if not self.wildcard_slot.has_card():
+            for from_idx in range(0, 9):
+                field_slot = self.field_slots[from_idx]
+                if len(field_slot) > 1:
+                    last_card = field_slot[-1]
+                    prev_card = field_slot[-2]
+                    if not self._can_move_cards([last_card], [prev_card]):
+                        valid_moves.append(Move(from_idx, -1, 1))
+        return valid_moves
+
+    def _can_move_cards(self, cards: List[Card], to_slot: List[Card]) -> bool:
+        def is_decending_values(cards: List[Card]) -> bool:
+            values = [card.value for card in cards]
+            for i in range(0, len(values) - 1):
+                if values[i]-1 != values[i+1]:
+                    return False
+            return True
+
+        def is_alternating_color(cards: List[Card]) -> bool:
+            if len(cards) == 1:
+                return True
+            reds = [card.is_red() for card in cards]
+            odd_reds = reds[::2] # should be of same color but opposite of even ones
+            even_reds = reds[1::2] # should be of same color
+            odd_reds_inv = [not red for red in odd_reds]
+            all_reds = odd_reds_inv + even_reds # should all be the same color
+            return all(red == all_reds[0] for red in all_reds)
+
+        # cards must be (all faces && same suit) or (cards must be all values and decending value and alternating color)
+        if not \
+            ((all([card.is_face() for card in cards]) and all([card.suit == cards[0].suit for card in cards])) \
+            or ((all([not card.is_face() for card in cards]) and is_decending_values(cards) and is_alternating_color(cards)))):
+            return False
+
+        # empty slot can accept any card (stack)
+        if not to_slot: 
+            return True
+        
+        to_card = to_slot[-1]
+        current_card = cards[0]
+        
+        if current_card.is_face() and to_card.is_face():
+            return to_card.suit == current_card.suit
+        elif not (current_card.is_face() or to_card.is_face()):
+            return (to_card.value == current_card.value + 1) and (current_card.is_red() != to_card.is_red())
+        else:
+            return False
 
     @staticmethod
     def _is_field_slot_done(cards: List[Card]) -> bool:
@@ -105,9 +238,8 @@ class Gameboard:
         if not cards:
             return True
         # all face, same suit
-        if all([card.is_face() for card in cards]):
-            suit = cards[0].suit
-            return all([card.suit == suit for card in cards])
+        if len(cards) == 4 and all([card.is_face() for card in cards]):
+            return all([card.suit == cards[0].suit for card in cards]) 
         # ordered values, interleaving color
         elif all([not card.is_face() for card in cards]):
             values = [card.value for card in cards]
@@ -120,6 +252,24 @@ class Gameboard:
         else:
             return False
 
+    def __str__(self):
+        return "|".join([str(self.wildcard_slot)] + [",".join([str(card) for card in field_slot]) for field_slot in self.field_slots])
+
+    @staticmethod
+    def from_str(source: str):
+        groups = source.split('|')
+        wildcard_str = groups[0]
+        field_slots_str = groups[1:]
+        field_slots: List[List[Card]] = []
+        for field_slot_str in field_slots_str:
+            field_slot: List[Card] = []
+            card_strs = field_slot_str.split(",")
+            for card_str in card_strs:
+                if card_str:
+                    field_slot.append(Card.from_str(card_str))
+            field_slots.append(field_slot)
+        return Gameboard(wildcard_slot=WildcardSlot.from_str(wildcard_str), field_slots=field_slots)
+
 class NacbracSolver(ABC):
     @abstractmethod
     def solve(self, gameboard: Gameboard) -> List[Move]:
@@ -127,22 +277,75 @@ class NacbracSolver(ABC):
 
 class DfsSolver(NacbracSolver):
     def solve(self, gameboard: Gameboard) -> List[Move]:
+        if not gameboard.validate():
+            raise ValueError("Invalid gameboard!")
+
+        self.hashes: Set = set()
+        self.gameboard = gameboard
+        self.last_update = datetime.datetime.now()
+        depth = 0
+        return self._solve(depth)
+
+    def _solve(self, depth: int) -> List[Move]:
         # dfs
-        #if solved(gameboard):
-            
-        # self.get_valid_moves(gameboard)
-        return []
+        if depth > 55:
+            return []
 
+        now = datetime.datetime.now()
+        if now > self.last_update + datetime.timedelta(seconds=60):
+            self.last_update = now
+            print(len(self.hashes))
 
+        def try_moves(moves: List[Move]) -> List[Move]:
+            for move in field_slot_moves:
+                self.gameboard.execute(move)
+                new_gameboard_hash = (str(self.gameboard))
+                if new_gameboard_hash not in self.hashes: # consider this new position
+                    self.hashes.add(new_gameboard_hash)
+                    if self.gameboard.solved():
+                        return [move]
+                    else:
+                        solution_moves = self._solve(depth + 1)
+                        if solution_moves:
+                            return [move] + solution_moves
+                # not a good move, revert
+                self.gameboard.undo(move)
+            return []
+        
+        field_slot_moves = self.gameboard.get_field_slot_moves()
+        solution = try_moves(field_slot_moves)
+
+        if solution:
+            return solution
+        
+        wildcard_slot_moves = self.gameboard.get_wildcard_slot_moves()
+        return try_moves(wildcard_slot_moves)
+
+SOME_INITIAL_GAMEBOARD = "_|8H,0C,0S,0S|0H,8D,0S,0D|10H,6H,10D,7H|0H,0S,9D,9H|7D,0H,0C,0H|9S,7C,8C,8S|0D,0D,0C,6D|6S,10S,0D,9C|7S,10C,6C,0C"
+
+def interactive_solution(solution: List[Move]):
+    for move in solution:
+        print("({} -> {})".format(move.before, move.after))
+        input()
+
+def pretty_format_solution(solution: List[Move]):
+    return ", ".join(["({} -> {})".format(move.before, move.after) for move in solution])
 
 def main():
     print("This is Nacbrac solver.")
 
-    gameboard: Gameboard = Gameboard(WildcardSlot(), [])
     solver: NacbracSolver = DfsSolver()
-    solution: List[Move] = solver.solve(gameboard)
+    starttime = datetime.datetime.now()
+    print("starttime {}".format(starttime))
 
-    print("{}".format(solution))
+    gameboard_str = SOME_INITIAL_GAMEBOARD
+    solution: List[Move] = solver.solve(Gameboard.from_str(gameboard_str))
+    
+    endtime = datetime.datetime.now()
+    print("endtime {}".format(endtime))
+    print("It took {}".format(endtime - starttime))
+    print("{} moves, solution [{}]".format(len(solution), pretty_format_solution(solution)))
+    #interactive_solution(solution)
 
 if __name__ == "__main__":
     main()
--- test_gameboard.py
+++ test_gameboard.py
@@ -1,85 +1,31 @@
 from unittest import TestCase
-from nacbrac import Gameboard, WildcardSlot, Card, Suit
+from nacbrac import Gameboard, WildcardSlot, Card, Suit, Move
+
+SOME_INITIAL_GAMEBOARD = "_|8H,0C,0S,0S|0H,8D,0S,0D|10H,6H,10D,7H|0H,0S,9D,9H|7D,0H,0C,0H|9S,7C,8C,8S|0D,0D,0C,6D|6S,10S,0D,9C|7S,10C,6C,0C"
 
 class TestGameboard(TestCase):
+    def setUp(self):
+        self.gameboard = Gameboard.from_str(SOME_INITIAL_GAMEBOARD)
+
     def test__validate__valid_board(self):
-        field_slots = [
-            [
-                Card(8, Suit.Heart),
-                Card(0, Suit.Club),
-                Card(0, Suit.Spade),
-                Card(0, Suit.Spade),
-            ],
-            [
-                Card(0, Suit.Heart),
-                Card(8, Suit.Diamond),
-                Card(0, Suit.Spade),
-                Card(0, Suit.Diamond),
-            ],
-            [
-                Card(10, Suit.Heart),
-                Card(6, Suit.Heart),
-                Card(10, Suit.Diamond),
-                Card(7, Suit.Heart),
-            ],
-            [
-                Card(0, Suit.Heart),
-                Card(0, Suit.Spade),
-                Card(9, Suit.Diamond),
-                Card(9, Suit.Heart),
-            ],
-            [
-                Card(7, Suit.Diamond),
-                Card(0, Suit.Heart),
-                Card(0, Suit.Club),
-                Card(0, Suit.Heart),
-            ],
-            [
-                Card(9, Suit.Spade),
-                Card(7, Suit.Club),
-                Card(8, Suit.Club),
-                Card(8, Suit.Spade),
-            ],
-            [
-                Card(0, Suit.Diamond),
-                Card(0, Suit.Diamond),
-                Card(0, Suit.Club),
-                Card(6, Suit.Diamond),
-            ],
-            [
-                Card(6, Suit.Spade),
-                Card(10, Suit.Spade),
-                Card(0, Suit.Diamond),
-                Card(9, Suit.Club),
-            ],
-                        [
-                Card(7, Suit.Spade),
-                Card(10, Suit.Club),
-                Card(6, Suit.Club),
-                Card(0, Suit.Club),
-            ],
-        ]
-        gameboard = Gameboard(WildcardSlot(), field_slots)
-        self.assertTrue(gameboard.validate())
+        self.assertTrue(self.gameboard.validate())
 
     def test__is_field_slot_done__valide(self):
-        gameboard = Gameboard(WildcardSlot(), [])
-
-        self.assertTrue(gameboard._is_field_slot_done([]))
-        self.assertTrue(gameboard._is_field_slot_done([
+        self.assertTrue(Gameboard._is_field_slot_done([]))
+        self.assertTrue(Gameboard._is_field_slot_done([
             Card(0, Suit.Heart),
             Card(0, Suit.Heart),
             Card(0, Suit.Heart),
             Card(0, Suit.Heart),
         ]))
-        self.assertTrue(gameboard._is_field_slot_done([
+        self.assertTrue(Gameboard._is_field_slot_done([
             Card(10, Suit.Heart),
             Card(9, Suit.Spade),
             Card(8, Suit.Diamond),
             Card(7, Suit.Spade),
             Card(6, Suit.Heart),
         ]))
-        self.assertTrue(gameboard._is_field_slot_done([
+        self.assertTrue(Gameboard._is_field_slot_done([
             Card(10, Suit.Club),
             Card(9, Suit.Diamond),
             Card(8, Suit.Spade),
@@ -88,32 +34,55 @@ class TestGameboard(TestCase):
         ]))
 
     def test__is_field_slot_done__invalide(self):
-        gameboard = Gameboard(WildcardSlot(), [])
-
-        self.assertFalse(gameboard._is_field_slot_done([
+        self.assertFalse(Gameboard._is_field_slot_done([
             Card(0, Suit.Club),
             Card(0, Suit.Heart),
             Card(0, Suit.Heart),
             Card(0, Suit.Heart),
         ]))
-        self.assertFalse(gameboard._is_field_slot_done([
+        self.assertFalse(Gameboard._is_field_slot_done([
             Card(10, Suit.Spade),
             Card(9, Suit.Spade),
             Card(8, Suit.Diamond),
             Card(7, Suit.Spade),
             Card(6, Suit.Heart),
         ]))
-        self.assertFalse(gameboard._is_field_slot_done([
+        self.assertFalse(Gameboard._is_field_slot_done([
             Card(10, Suit.Club),
             Card(9, Suit.Diamond),
             Card(8, Suit.Spade),
             Card(7, Suit.Heart),
             Card(7, Suit.Spade),
         ]))
-        self.assertFalse(gameboard._is_field_slot_done([
+        self.assertFalse(Gameboard._is_field_slot_done([
             Card(6, Suit.Club),
             Card(9, Suit.Diamond),
             Card(8, Suit.Spade),
             Card(7, Suit.Heart),
             Card(10, Suit.Spade),
         ]))
+
+    def test__solved__initial_board(self):
+        self.assertFalse(self.gameboard.solved())
+
+    def test__execute_undo__noop(self):
+        initial_hash = hash(str(self.gameboard))
+        move = Move(before=5, after=3, num_cards=1)
+        self.gameboard.execute(move)
+        self.assertNotEqual(initial_hash, hash(str(self.gameboard)))
+        self.gameboard.undo(move)
+        self.assertEqual(initial_hash, hash(str(self.gameboard)))
+
+    def test__get_field_slot_moves__happy_case(self):
+        expected = [Move(before=2, after=5, num_cards=1), Move(before=5, after=3, num_cards=1)]
+        self.assertEqual(expected, self.gameboard.get_field_slot_moves())
+
+        self.gameboard = Gameboard.from_str("_|10D,9C,8D,7C,6D|0H,0H,0H|10H,9S,8H,7S,6H|0H|10S,9D,8C,7D,6C|0S,0S,0S,0S|0D,0D,0D,0D|10C,9H,8S,7H,6S|0C,0C,0C,0C")
+        expected = [Move(before=1, after=3, num_cards=3), Move(before=3, after=1, num_cards=1)]
+        self.assertEqual(expected, self.gameboard.get_field_slot_moves())
+
+    def test__wildcard_slot_moves__valid_moves(self):
+        self.gameboard = Gameboard.from_str("_|10D,9C,8D,7C|0H,0H,0H,6D|10H,9S,8H,7S,6H|0H|10S,9D,8C,7D,6C|0S,0S,0S,0S|0D,0D,0D,0D|10C,9H,8S,7H,6S|0C,0C,0C,0C")
+        expected = [Move(before=1, after=-1, num_cards=1)]
+        self.assertEqual(expected, self.gameboard.get_wildcard_slot_moves())
+        
